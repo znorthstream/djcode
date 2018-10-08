3daff46461a78f5d8d5ac801a407d2c3714ca744
DESKTOP-P8NVTRV\magistr <x2wing@gmail.com>
2018-10-08 21:08:23 +0500

    synopsis was added

--- mysite/test.py
+++ mysite/test.py
@@ -2,6 +2,15 @@
 команда запуска
 exec(open('mysite/test.py', encoding='utf8').read())
 
+выполнить миграцию всех баз
+python manage.py migrate
+
+валидация схемы модели
+ВНИМАНИЕ приложение должно быть добавлено в INSTALLED_APPS
+python manage.py check
+
+перенести схему в БД
+python manage.py makemigrations books
 '''
 from django import template
 # from django.conf import settings
